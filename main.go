75d639f9c2d16a2db123a9030c69b523d4d9f92a
Arocano <92830653+Arocano@users.noreply.github.com>
2023-09-29 08:48:23 -0500

    limite ideal

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/gorilla/mux"
 
@@ -14,9 +15,8 @@ import (
 
 type sentence struct {
 	Worlds	    string  `json:worlds`
-	Frecuency   float64 `json:frecuency`
-	Limit       float32 `json:limit`
-	Ideal_limit float32 `json:ideal`
+	Limit       float64 `json:limit`
+	Ideal_limit float64 `json:ideal`
 }
 
 type allsentences []sentence
@@ -24,7 +24,6 @@ type allsentences []sentence
 var sentences = allsentences{
 	{
 		Worlds:      "Hello",
-		Frecuency:   0.5,
 		Limit:       0.5,
 		Ideal_limit: 0.5,
 	},
@@ -34,6 +33,41 @@ func getSentences(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(sentences)
 }
+func contarLetras(cadena string) map[rune]int {
+	resultado := make(map[rune]int)
+
+	// Convierte la cadena a minúsculas para que no distinga entre mayúsculas y minúsculas
+	cadena = strings.ToLower(cadena)
+
+	// Recorre la cadena y cuenta las letras
+	for _, char := range cadena {
+		if 'a' <= char && char <= 'z' {
+			resultado[char]++
+		}
+	}
+
+	return resultado
+}
+func dividirConteo(resultados map[rune]int, divisor float64) map[rune]float64 {
+	resultadoDividido := make(map[rune]float64)
+
+	// Itera sobre los resultados y divide el conteo por el divisor
+	for letra, count := range resultados {
+		resultadoDividido[letra] = float64(count) / float64(divisor)
+	}
+
+	return resultadoDividido
+}
+func sumarValores(resultadosDivididos map[rune]float64) float64 {
+	suma := 0.0
+
+	// Itera sobre los valores y suma
+	for _, valor := range resultadosDivididos {
+		suma += valor
+	}
+
+	return suma
+}
 
 func createSentence(w http.ResponseWriter, r *http.Request) {
 	var newSentence sentence
@@ -42,9 +76,37 @@ func createSentence(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Insert a valid sentence")
 	}
 	json.Unmarshal(reqBody, &newSentence)
+
+	worldsWhtoutSpace := strings.ReplaceAll(newSentence.Worlds, " ", "")
+	fmt.Println(worldsWhtoutSpace)
+
+	newSentence.Limit= float64(len(worldsWhtoutSpace))
+	fmt.Println(newSentence.Limit)
+
+	resultado := contarLetras(worldsWhtoutSpace)
+
+	for letra, count := range resultado {
+		fmt.Printf("'%c': %d\n", letra, count)
+	}
+
+	resultadoDividido := dividirConteo(resultado, newSentence.Limit )
+	for letra, count := range resultadoDividido {
+		fmt.Printf("'%c': %.16f\n", letra, count)
+	}
+
+	suma := sumarValores(resultadoDividido)
+
+	fmt.Printf("Suma de valores divididos: %.16f\n", suma)
+
+	newSentence.Ideal_limit= suma
+
 	sentences = append(sentences, newSentence)
+
+
 	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(http.StatusCreated)
+
 	json.NewEncoder(w).Encode(newSentence)
 }
 func indexRoute(w http.ResponseWriter, r *http.Request) {
