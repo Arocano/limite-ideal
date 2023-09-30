eddc3765a909b8d2ff3102eda5b7d464f059d2a0
Arocano <92830653+Arocano@users.noreply.github.com>
2023-09-30 16:46:40 -0500

    terminado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import './App.css';
 function App() {
   const [Words, setSentence] = useState('');
   const [limit, setLimit] = useState('');
@@ -7,12 +7,12 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const  response = await fetch(import.meta.env.VITE_API +'/sentences',{
+    const response = await fetch(import.meta.env.VITE_API + '/sentences', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({Words})
+      body: JSON.stringify({ Words })
     })
     const data = await response.json()
     setLimit(data.Ideal_limit);
@@ -21,18 +21,23 @@ function App() {
   }
   return (
     <div>
-<form onSubmit={handleSubmit}>
-  <input type="sentence" placeholder="Ingresa la oración" onChange={(e)=>setSentence(e.target.value)}/>
-  <button>Calcular</button>
-</form>
-     <div>
+      <div className="centered-text">
+        <h1 className='header-text' >Calculadora del límite ideal</h1>
+      </div>
 
-        <h1>El limite ideal es: {limit}</h1>
-     </div>
-    
+      <form  className ="form" onSubmit={handleSubmit}>
+        <label className='label'>Ingrese el texto:</label>
+        <textarea className='input' type="sentence" placeholder="hola mundo" onChange={(e) => setSentence(e.target.value)} />
+        <button className='button'>Calcular</button>
+      </form>
+      <div className='limit'>
+        <h2>El límite ideal es:</h2>
+        <p className='pa'> {limit}</p>
+      </div>
+
     </div>
-     
-     )
+
+  )
 
 }
 
--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"limite/models"
-	"net/http"
 	"strings"
-
+	"golang.org/x/text/unicode/norm"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"unicode"
+
 )
 
 type allsentences []models.Sentence
@@ -22,18 +21,41 @@ var sentences = allsentences{
 	},
 }
 
-func getSentences(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	json.NewEncoder(w).Encode(sentences)
+func quitarAcentos(cadena string) string {
+	// Crea una forma de normalización NFD
+	forma := norm.NFD
+
+	// Aplica la normalización para descomponer los caracteres
+	cadenaNormalizada := forma.String(cadena)
+
+	// Elimina los caracteres de marca diacrítica
+	cadenaSinAcentos := strings.Map(func(r rune) rune {
+		if r >= 'A' && r <= 'Z' {
+			// Mantener letras mayúsculas sin cambios
+			return r
+		}
+		if r >= 'a' && r <= 'z' {
+			// Mantener letras minúsculas sin cambios
+			return r
+		}
+		if unicode.Is(unicode.Mn, r) {
+			// Eliminar marcas diacríticas
+			return -1
+		}
+		return r
+	}, cadenaNormalizada)
+
+	return cadenaSinAcentos
 }
+
 func contarLetras(cadena string) map[rune]int {
 	resultado := make(map[rune]int)
+
+	
 
 	// Convierte la cadena a minúsculas para que no distinga entre mayúsculas y minúsculas
 	cadena = strings.ToLower(cadena)
 
-	// Recorre la cadena y cuenta las letras
 	for _, char := range cadena {
 		if 'a' <= char && char <= 'z' {
 			resultado[char]++
@@ -63,50 +85,6 @@ func sumarValores(resultadosDivididos map[rune]float64) float64 {
 	return suma
 }
 
-func createSentence(w http.ResponseWriter, r *http.Request) {
-	var newSentence models.Sentence
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Insert a valid sentence")
-	}
-	json.Unmarshal(reqBody, &newSentence)
-
-	worldsWhtoutSpace := strings.ReplaceAll(newSentence.Words, " ", "")
-	fmt.Println(worldsWhtoutSpace)
-
-	newSentence.Limit = float64(len(worldsWhtoutSpace))
-	fmt.Println(newSentence.Limit)
-
-	resultado := contarLetras(worldsWhtoutSpace)
-
-	for letra, count := range resultado {
-		fmt.Printf("'%c': %d\n", letra, count)
-	}
-
-	resultadoDividido := dividirConteo(resultado, newSentence.Limit)
-	for letra, count := range resultadoDividido {
-		fmt.Printf("'%c': %.16f\n", letra, count)
-	}
-
-	suma := sumarValores(resultadoDividido)
-
-	fmt.Printf("Suma de valores divididos: %.16f\n", suma)
-
-	newSentence.Ideal_limit = suma
-
-	sentences = append(sentences, newSentence)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	w.WriteHeader(http.StatusCreated)
-
-	json.NewEncoder(w).Encode(newSentence)
-}
-func indexRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my API ")
-}
-
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
@@ -119,7 +97,6 @@ func main() {
 	app.Post("/sentences", func(c *fiber.Ctx) error {
 		newSentence := new(models.Sentence)
 
-		
 		if err := c.BodyParser(newSentence); err != nil {
 			fmt.Println("error = ", err)
 			return c.SendStatus(200)
@@ -127,11 +104,12 @@ func main() {
 		c.BodyParser(&newSentence)
 		wordsWhtoutSpace := strings.ReplaceAll(newSentence.Words, " ", "")
 		fmt.Println(wordsWhtoutSpace)
-
-		newSentence.Limit = float64(len(wordsWhtoutSpace))
+        cnew:=quitarAcentos(wordsWhtoutSpace)
+		fmt.Println(cnew)
+		newSentence.Limit = float64(len(cnew))
 		fmt.Println(newSentence.Limit)
 
-		resultado := contarLetras(wordsWhtoutSpace)
+		resultado := contarLetras(cnew)
 
 		for letra, count := range resultado {
 			fmt.Printf("'%c': %d\n", letra, count)
@@ -150,12 +128,6 @@ func main() {
 
 		return c.JSON(newSentence)
 	})
-	//router := mux.NewRouter().StrictSlash(true)
-	//router.HandleFunc("/", indexRoute)
-	//router.HandleFunc("/sentences", getSentences).Methods("GET")
-	//router.HandleFunc("/sentences", createSentence).Methods("POST")
-	//log.Fatal(http.ListenAndServe(":3000", router))
-
 	app.Listen(":3000")
 
 }
